dc195882bfaedb326653430abb0d6594d9612a4e
Bermuda_UT <hoso1312@gmail.com>
2016-07-30 13:08:49 +1000

    flow router installation and others

--- client/main.js
+++ client/main.js
@@ -1,12 +1,7 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+import './templates/main.html';
 
 Template.hello.helpers({
   counter() {
@@ -20,3 +15,10 @@ Template.hello.events({
     instance.counter.set(instance.counter.get() + 1);
   },
 });
+
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
+});
+
+
