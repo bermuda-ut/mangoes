d4d173cb4d94bf6abea7b631d15a9682cde79fa6
fourblank <jfong361@gmail.com>
2016-07-30 19:10:43 +1000

    recipe show page

--- client/templates/recipe/recipe.js
+++ client/templates/recipe/recipe.js
@@ -1,12 +1,36 @@
+import {Session} from 'meteor/session';
+
 Template.recipe.events({
     'submit .recipe-form': function(event) {
         event.preventDefault();
         let ing = event.target.text.value;
         Meteor.call('getRecipe', ing, function(e, res) {
-            let results = JSON.parse(res.content).results;
-            for (let i = 0; i < results.length; i++) {
-                console.log(results[i].title);
-            }
+            let results = _.uniq(JSON.parse(res.content).results, function(p) {return p.title});
+            results = _.filter(results, function(p) {return p.href.indexOf("kraft") == -1;});
+            Session.set("loaded", true);
+            Session.set("results", results);
         });
     }
+});
+
+Template.showRecipes.helpers({
+    recipes() {
+        return Session.get("results");       
+    },
+
+    canShow() {
+        return Session.get("loaded");
+    }
+    
+});
+
+Template.showRecipes.onCreated(function() {
+    Session.set("loaded", false);
+    let ing = "chicken,avocado"; // TODO: this is hardcoded
+    Meteor.call('getRecipe', ing, function(e, res) {
+        let results = _.uniq(JSON.parse(res.content).results, function(p) {return p.title});
+        results = _.filter(results, function(p) {return p.href.indexOf("kraft") == -1;});
+        Session.set("loaded", true);
+        Session.set("results", results);
+    });
 });
