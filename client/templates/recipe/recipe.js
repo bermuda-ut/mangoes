9eac491ff4cac2d44a134555360558e05da61dad
fourblank <jfong361@gmail.com>
2016-07-30 23:32:37 +1000

    recipe popup according to data

--- client/templates/recipe/recipe.js
+++ client/templates/recipe/recipe.js
@@ -1,36 +1,55 @@
 import {Session} from 'meteor/session';
 
-Template.recipe.events({
-    'submit .recipe-form': function(event) {
-        event.preventDefault();
-        let ing = event.target.text.value;
-        Meteor.call('getRecipe', ing, function(e, res) {
-            let results = _.uniq(JSON.parse(res.content).results, function(p) {return p.title});
-            results = _.filter(results, function(p) {return p.href.indexOf("kraft") == -1;});
-            Session.set("loaded", true);
-            Session.set("results", results);
-        });
-    }
-});
-
 Template.showRecipes.helpers({
     recipes() {
-        return Session.get("results");       
-    },
-
-    canShow() {
-        return Session.get("loaded");
+        let ingre = Fridge.find().fetch();
+        let ing = [];
+        for (let i = 0; i < Fridge.find().count(); i++) {
+            ing.push(ingre[i].name);
+        }
+        ing = ing.join();
+        Meteor.call('getRecipe', ing, function(e, res) {
+            let results = _.uniq(JSON.parse(res.content).results, function(p) {return p.title});
+            results = _.filter(results, function(p) {
+                return p.thumbnail != "" && p.href.indexOf("kraft") == -1;
+            });
+            Session.set("results", results);
+        });
+        return Session.get("results");
     }
-    
 });
 
-Template.showRecipes.onCreated(function() {
-    Session.set("loaded", false);
-    let ing = "chicken,avocado"; // TODO: this is hardcoded
-    Meteor.call('getRecipe', ing, function(e, res) {
-        let results = _.uniq(JSON.parse(res.content).results, function(p) {return p.title});
-        results = _.filter(results, function(p) {return p.href.indexOf("kraft") == -1;});
-        Session.set("loaded", true);
-        Session.set("results", results);
-    });
+Template.singleRecipe.helpers({
+    recipe() {
+        let ingre = Fridge.find().fetch();
+        let ing = [];
+        for (let i = 0; i < Fridge.find().count(); i++) {
+            ing.push(ingre[i].name);
+        }
+        ing = ing.join();
+        Meteor.call('getRecipe', ing, function(e, res) {
+            let results = _.uniq(JSON.parse(res.content).results, function(p) {return p.title});
+            results = _.filter(results, function(p) {
+                return p.thumbnail != "" && p.href.indexOf("kraft") == -1;
+            });
+            Session.set("results", results);
+        });
+        return Session.get("results")[0];
+    }
+});
+
+Template.recipeOfTheDay.helpers({
+	showAll() {
+		return Session.get('showAll');
+	}
+});
+
+Template.recipeOfTheDay.events({
+    'click .show-all-btn': function() {
+        Session.set('showAll', !Session.get('showAll'));
+    }
+});
+
+Template.recipeOfTheDay.onCreated(function() {
+    Session.set('showAll', false);
 });
