3f00b93409b0741fe4e6bf712946b0ea0ebd0458
Bermuda_UT <hoso1312@gmail.com>
2016-07-31 05:04:53 +1000

    completed cycle

--- arduino/python2mongo.py
+++ arduino/python2mongo.py
@@ -6,9 +6,9 @@ import sys
 # Setup serial
 s = serial.Serial(port='/dev/ttyACM0', baudrate=9600)
 
-def insert(w):
-    coll.insert_one({'name': w})
-    print "Inserted {}".format(w)
+def insert(name, date):
+    coll.insert_one({'name': name, 'doe': date})
+    print "Inserted {}".format(name)
 
 print "Connecting to mongoDB"
 try:
@@ -20,14 +20,36 @@ except:
     exit(1)
     
 # Run the read() command while true 
+print "Ready"
+
 while True:
-    print "ready"
-    str = s.readline();
-    print str;
+    raw = s.readline();
     
     # Ignore new line characters
-    if len(str) > 2:
-        productHex   = str.replace(' ', '');
-        productHex   = productHex.partition("F")[0];
-        productASCII = binascii.a2b_hex(productHex);
-        insert(productASCII)
+    if len(raw) > 2 and len(raw) % 2 == 0:
+        try:
+            print raw;
+            productHex = raw.strip().replace(' ', '');
+            productASCII = productHex.decode('hex')
+
+            productDate = filter(str.isdigit, productASCII)
+            productName = filter(str.isalpha, productASCII)
+
+            insert(productName.strip(), productDate.strip())
+        except:
+            print "Failed to read"
+
+        print "Ready"
+
+"""
+hardware wiring:
+
+from   to
+-------------
+reset  pin9
+ground ground
+miso   pin12
+mosi   pin11
+sck    pin13
+nss    pin10
+"""
---
+++ client/templates/addFridge.js
@@ -0,0 +1,35 @@
+Template.addFridge.helpers({
+    myArr() {
+        return TempFridge.find({},{
+            sort: {
+                'name': 1
+            }
+        })
+    }
+});
+
+Template.addFridge.events({
+    'click .nowAdd': function() {
+        var toAdd = TempFridge.find().fetch();
+
+        for(i = 0; i < toAdd.length; i++) {
+            var today = new Date();
+
+            var exp = today.setDate(today.getDate() + Math.floor((Math.random() * 14) + 1));
+
+            if(toAdd[i].doe)
+                exp = toAdd[i].doe
+
+            var doc = {
+                name: toAdd[i].name,
+                doe: exp
+            }
+            Meteor.call('addToFridge', doc);
+        }
+
+        FlowRouter.go('/');
+    }
+});
+
+Template.addFridge.onCreated(function addFridgeOnCreated() {
+});
